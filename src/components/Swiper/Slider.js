915e39fb3fdf764ad1ae3147d0f7876180f3a220
Jangmyun <cndlsrb2739@gmail.com>
2024-07-02 11:32:38 +0900

    slider alt img 추가

--- src/components/Swiper/Slider.js
+++ src/components/Swiper/Slider.js
@@ -4,7 +4,7 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { useEffect, useState } from "react";
 
 /**
- * [Component] Slider
+ * [Component] Slider : 공연 포스터 auto play 캐러셀
  */
 function Slider(props) {
   // 현재 공연 일정이 얼마 남지 않은 공연 리스트 저장하는 state
@@ -15,8 +15,6 @@ function Slider(props) {
     const json = await (await fetch("dummyData/movies.json")).json();
     setConcerts(json);
   }
-
-  //
   useEffect(() => {
     getMovies();
     console.log(concerts);
@@ -24,25 +22,25 @@ function Slider(props) {
 
   return (
     <Swiper
-      observer={true}
       modules={[Navigation, Pagination, Autoplay]}
       navigation
       autoplay={{ delay: 2000 }}
       spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => {
-        console.log("slide change");
-      }}
       onSwiper={(swiper) => {
         console.log(swiper);
       }}
     >
       {concerts.map((item, index) => (
         <SwiperSlide key={item.title}>
+          {item.url != null ? (
+            <img src={item.url} alt="url" />
+          ) : (
+            <img width={"100px"} src={"/img/loadImage.png"} alt="" />
+          )}
           <h3>
             {item.title} ({item.year})
           </h3>
-          <p>{item.plot}</p>
         </SwiperSlide>
       ))}
     </Swiper>
