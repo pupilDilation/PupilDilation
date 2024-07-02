d79f5a806c0d92e56ba9f459282146d314441bc2
Jangmyun <cndlsrb2739@gmail.com>
2024-07-02 21:43:10 +0900

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Main from "./pages/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main></Main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main></Main>}></Route>
+          <Route path="/detail" element={<div>Detail Page</div>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
