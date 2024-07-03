42fc626f0134fd23a86b38f1f354b09a476f75bb
Jangmyun <cndlsrb2739@gmail.com>
2024-07-03 16:27:41 +0900

    [feat#2] useClassNameJoin 훅 이름 변경

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Slider from "../components/Swiper/Slider";
+import useClassNameJoin from "../hooks/useClassNameJoin";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import Slider from "../components/Swiper/Slider";
+import useClassNameJoin from "../hooks/useClassNameJoin";
+
+function Main() {
+  return (
+    <div>
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default Main;
