148590ad19ef9dfb8b7d59d63fd4d093b49c6c9c
Jangmyun <cndlsrb2739@gmail.com>
2024-07-03 15:52:50 +0900

    [feat#2] react-router-dom으로부터 Route 컴포넌트 import 및 Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import Main from "./pages/Main";
+import LoginPage from "./pages/Login";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/detail" element={<div>Detail Page</div>}></Route>
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
