0cb1273ad975e8ec9c6da8b8a82fc54c47664000
Jangmyun <cndlsrb2739@gmail.com>
2024-07-03 10:04:29 +0900

    slider style 적용

--- src/components/Swiper/Slide.js
+++ src/components/Swiper/Slide.js
@@ -5,14 +5,14 @@ function Slide({ item }) {
   return (
     <Link to={"/detail"} className={styles.sliderLink}>
       <div className={styles.sliderContainer}>
-        <div>
+        <div className={styles.sliderImgContainer}>
           {item.imgURL != null ? (
             <img src={item.imgURL} alt={item.title} />
           ) : (
             <img src={"/img/loadImage.png"} alt={item.title} />
           )}
         </div>
-        <dl className={styles.slideContent}>
+        <dl className={styles.slideInfo}>
           <dt>{item.title}</dt>
           <dd>{item.venue}</dd>
           <dd>{item.screening_period}</dd>
