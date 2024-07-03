4fb57b84761b5189ed7111de433a8fb3568158ae
Jangmyun <cndlsrb2739@gmail.com>
2024-07-03 15:56:01 +0900

    [feat#2] Main 컴포넌트 구성

---
+++ src/routes/Main.js
@@ -0,0 +1,12 @@
+import Slider from "../components/Swiper/Slider";
+import useClassNameJoin from "../hooks/useClassName";
+
+function Main() {
+  return (
+    <div>
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default Main;
