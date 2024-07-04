1acc8d8aa077f4d0e5ec4ee1ae9c0a034bd241b5
248Kobe <ecjung077@gmail.com>
2024-07-04 18:17:31 +0900

    [feat #11] Wrapper 생성

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
 import Header from "../components/Header/Header";
 import Slider from "../components/Swiper/Slider";
 import useClassNameJoin from "../hooks/useClassNameJoin";
+import Wrapper from "../components/Wrapper/Wrapper";
+import WrapperStyles from "../components/Wrapper/Wrapper.module.css";
 
 function LoginPage() {
   return (
-    <div>
+    <Wrapper>
       <h1>Login Page</h1>
-    </div>
+    </Wrapper>
   );
 }
 
