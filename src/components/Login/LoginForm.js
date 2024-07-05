bab9278143970505bf880b3b10254d144b3afc8f
hhaminlee <hamin6899@naver.com>
2024-07-05 14:36:24 +0900

    [feat #10] LoginForm Button 수정

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,28 +1,31 @@
 import React from "react";
 import Input from "../Input/Input";
-import InputStyles from "../Login/LoginForm.module.css";
+import LoginFormStyles from "../Login/LoginForm.module.css";
 import Button from "../Button/Button";
 import Buttonstyles from "../Button/Button.module.css";
+import useClassNameJoin from "../../hooks/useClassNameJoin";
 
 function LoginForm() {
   return (
-    <div className={InputStyles.loginWrapper}>
-      <div className={InputStyles.loginText}>
+    <div className={LoginFormStyles.loginWrapper}>
+      <div className={LoginFormStyles.loginText}>
         <h5>로그인</h5>
       </div>
-      <div className={InputStyles.inputWrapper}>
-        <Input className={InputStyles.input1} placeholder={"아이디"} />
-        <Input className={InputStyles.input1} placeholder={"비밀번호"} />
-        <div className={InputStyles.saveIdCheckBox}>
+      <div className={LoginFormStyles.inputWrapper}>
+        <Input className={LoginFormStyles.input1} placeholder={"아이디"} />
+        <Input className={LoginFormStyles.input1} placeholder={"비밀번호"} />
+        <div className={LoginFormStyles.saveIdCheckBox}>
           <input type="checkbox" id="saveIdCheckBox" />
           <label htmlFor="saveIdCheckBox">아이디 저장</label>
         </div>
       </div>
-      <Button className={Buttonstyles.loginBtn}>
-        <div className={InputStyles.loginBtnText}>로그인</div>
-      </Button>
-      <Button className={Buttonstyles.signUpBtn}>
-        <div className={InputStyles.signUpBtnText}>회원가입</div>
+      <Button
+        className={useClassNameJoin(
+          Buttonstyles.buttonCommon,
+          Buttonstyles.loginBtn
+        )}
+      >
+        <div className={LoginFormStyles.loginBtnText}>로그인</div>
       </Button>
     </div>
   );
