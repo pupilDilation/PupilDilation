8393e594625da2dfc85869eae7c485ae21604eda
Jangmyun <cndlsrb2739@gmail.com>
2024-07-08 15:23:18 +0900

    [feat #20] App.js RouterWrapper / RoutesWrapper 생성

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import LoginPage from "./pages/Login";
 import SignUpPage from "./pages/SignUp";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Wrapper from "./components/Wrapper/Wrapper";
+import WrapperStyles from "./components/Wrapper/Wrapper.module.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/detail" element={<div>Detail Page</div>}></Route>
-        </Routes>
-      </Router>
-      <Footer />
+      <Wrapper className={WrapperStyles.routerWrapper}>
+        <Router>
+          <Header />
+          <Wrapper className={WrapperStyles.routesWrapper}>
+            <Routes>
+              <Route path="/" element={<Main />}></Route>
+              <Route path="/login" element={<LoginPage />}></Route>
+              <Route path="/signup" element={<SignUpPage />}></Route>
+              <Route path="/detail" element={<div>Detail Page</div>}></Route>
+            </Routes>
+          </Wrapper>
+          <Footer />
+        </Router>
+      </Wrapper>
     </div>
   );
 }
