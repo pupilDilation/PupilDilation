4110df73727fa80b07497938c8fa9afa5c71b75d
hhaminlee <hamin6899@naver.com>
2024-07-11 01:43:00 +0900

    [feat #30] seatSlice.js 주석 추가, app.js seat 라우트 추가

--- src/slice/seatSlice.js
+++ src/slice/seatSlice.js
@@ -9,7 +9,8 @@ const seatsSlice = createSlice({
   initialState,
   reducers: {
     toggleSeat: (state, action) => {
-      const seatNumber = action.payload;
+      const seatNumber = action.payload; //좌석 번호 추출
+      /* 선택된 좌석 목록에서 해당 좌석이 포함되었는지 판단 -> 선택, 선택 해제 */
       if (state.selectedSeats.includes(seatNumber)) {
         state.selectedSeats = state.selectedSeats.filter(
           (seat) => seat !== seatNumber
