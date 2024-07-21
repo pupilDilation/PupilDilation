5e47762dfaf6a8d711cb935a4f3bab834f4a8539
Jangmyun <cndlsrb2739@gmail.com>
2024-07-21 19:11:53 +0900

    [feat #66]sequelize 사용부분 임시 주석처리, concertRoutes 연결

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // express rest api 생성을 위한 템플릿
 const express = require("express");
+const concertRoutes = require("./routes/concertRoutes");
 const path = require("path");
 const app = express();
 const port = 3001;
 
+// sequelize db models configuration files
+// const db = require("./models");
+
 //mysql2 db configuration file
-const db1 = require("./config/DB");
+const db1 = require("./config/dbConfig");
 
 const { myQ } = require("./config/queries");
 
@@ -13,6 +17,7 @@ const { myQ } = require("./config/queries");
 // client가 서버로 보내는 요청이 json 형식일 때
 // javascript 오브젝트로 변환해서 req.body 에 저장
 app.use(express.json());
+app.use("/concerts", concertRoutes);
 
 app.get("/concerts", async (req, res) => {
   try {
@@ -23,14 +28,12 @@ app.get("/concerts", async (req, res) => {
   }
 });
 
-const db = require("./models");
-
 // Routers
-const userRouter = require("./routes/Users");
-app.use("/users", userRouter);
+// const userRouter = require("./routes/Users");
+// app.use("/users", userRouter);
 
-const concertRouter = require("./routes/Concerts");
-app.use("/concert", concertRouter);
+// const concertRouter = require("./routes/Concerts");
+// app.use("/concert", concertRouter);
 
 app.use(express.static(path.join(__dirname, "pupil/build")));
 
@@ -38,8 +41,12 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "pupil/build/index.html"));
 });
 
-db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server Running on Port ${port}`);
-  });
+// db.sequelize.sync().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server Running on Port ${port}`);
+//   });
+// });
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
 });
