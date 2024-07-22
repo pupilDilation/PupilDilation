26ef0346f78563c39f6c7dbeec2be8cbb6bd9556
Jangmyun <cndlsrb2739@gmail.com>
2024-07-22 12:38:04 +0900

    [fix #66] 일주일 전 공연정보 가져오는 쿼리 동작 안하는 문제 수정 (backtick 을 사용해서 쿼리문 보기좋게 작성)

--- models/concertModel.js
+++ models/concertModel.js
@@ -4,9 +4,34 @@ const db = require("../config/dbConfig");
  * 상영 예정이 7일 이내로 남은 session의 concert_id와 session_date 가져오기
  */
 const getCurrentConcerts = async () => {
-  const [rows] = await db.query(
-    "SELECT concert_id session_date FROM session WHERE concert_id = @concert_id AND session_date BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '7' DAY;"
-  );
+  const [rows] = await db.query(`
+    SELECT 
+      c.concert_id,
+      c.concert_title, 
+      c.concert_location,
+      c.concert_price,
+      c.concert_row,
+      c.concert_col, 
+      c.concert_img,
+      c.concert_plot, 
+      c.created_at, 
+      c.updated_at, 
+      c.deleted_at, 
+      c.user_id,
+      s.session_id,
+      s.session_date
+    FROM 
+        concert c
+    JOIN 
+        session s ON c.concert_id = s.concert_id
+    WHERE 
+        s.session_date >= NOW()
+        AND s.session_date <= DATE_ADD(NOW(), INTERVAL 1 WEEK)
+        AND c.deleted_at IS NULL
+        AND s.deleted_at IS NULL
+    ORDER BY 
+        s.session_date;
+    `);
   return rows;
 };
 
