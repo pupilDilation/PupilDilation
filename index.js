7c09f56068789195a47c3d7cd4988b5ce606c4c3
Jangmyun <cndlsrb2739@gmail.com>
2024-07-22 13:08:01 +0900

    [fix #66] 쿼리문 파일 삭제로 불필요한 코드 제거

--- index.js
+++ index.js
@@ -11,22 +11,11 @@ const port = 3001;
 //mysql2 db configuration file
 const db1 = require("./config/dbConfig");
 
-const { myQ } = require("./config/queries");
-
 // express가 json 형식 요청을 자동으로 파싱
 // client가 서버로 보내는 요청이 json 형식일 때
 // javascript 오브젝트로 변환해서 req.body 에 저장
 app.use(express.json());
 app.use("/concerts", concertRoutes);
-
-app.get("/concerts", async (req, res) => {
-  try {
-    const [row] = await db1.query(myQ);
-    res.json(row[0]);
-  } catch (err) {
-    console.log(err);
-  }
-});
 
 // Routers
 // const userRouter = require("./routes/Users");
