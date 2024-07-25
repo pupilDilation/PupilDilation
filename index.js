0dc81f895a7b18d2103184fb3c79954ab71b56f6
Jangmyun <cndlsrb2739@gmail.com>
2024-07-25 14:42:38 +0900

    [fix #72] sequelize 제거

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const app = express();
 const port = 3001;
 const cors = require("cors");
 
-// sequelize db models configuration files
-// const db = require("./models");
-
 //mysql2 db configuration file
 const db1 = require("./config/dbConfig");
 
@@ -19,27 +16,15 @@ const db1 = require("./config/dbConfig");
 // javascript 오브젝트로 변환해서 req.body 에 저장
 app.use(express.json());
 app.use(cors());
+// Routes 주소
 app.use("/concerts", concertRoutes);
 app.use("/users", userRoutes);
-
-// Routers
-// const userRouter = require("./routes/Users");
-// app.use("/users", userRouter);
-
-// const concertRouter = require("./routes/Concerts");
-// app.use("/concert", concertRouter);
 
 app.use(express.static(path.join(__dirname, "pupil/build")));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "pupil/build/index.html"));
 });
-
-// db.sequelize.sync().then(() => {
-//   app.listen(port, () => {
-//     console.log(`Server Running on Port ${port}`);
-//   });
-// });
 
 app.listen(port, () => {
   console.log(`Server Running on Port ${port}`);
