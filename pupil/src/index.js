a5e421781f83a5b4f734af5b8e37d315a249eba3
248Kobe <ecjung077@gmail.com>
2024-07-29 12:41:34 +0900

    [feat #75] 디렉토리 재설정, 주석추가

--- pupil/src/index.js
+++ pupil/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./store";
 import App from "./App";
-import './index.css';
-import './assets/fonts/Font.css';
+import "./index.css";
+import "./assets/fonts/Font.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- pupil/src/pages/MyPage.js
+++ pupil/src/pages/MyPage.js
@@ -6,6 +6,13 @@ import Wrapper from "../components/Wrapper/Wrapper";
 import WrapperStyles from "../components/Wrapper/Wrapper.module.css";
 import Ticket from "../components/Ticket/Ticket";
 
+/**
+ * @author: 248Kobe
+ * @returns 마이페이지에 들어갈 내용
+ * Wrapper 안에 회원 정보 + 예매 내역
+ * @description: 마이페이지에서 회원 정보, 예매 내역 조회
+ * 회원 정보 수정, 예매 내역 취소 기능 포함
+ */
 function MyPage() {
   return (
     <Wrapper className={WrapperStyles.myPageContainer}>
@@ -13,7 +20,7 @@ function MyPage() {
         <Button className={ButtonStyles.headerBtn}>회원정보 아이콘</Button>
         <Button className={ButtonStyles.headerBtn}>예매내역</Button>
       </div>
-      <UserInfo
+      <UserInfo //회원 정보 컴포넌트
         name="정은찬"
         id="chany077"
         email="chany077@gmail.com"
@@ -22,7 +29,7 @@ function MyPage() {
       <Wrapper className={WrapperStyles.wrapper}>
         <h1>예매 내역</h1>
       </Wrapper>
-      <Ticket
+      <Ticket //예매 티켓 컴포넌트
         title="제목"
         date="날짜"
         payment="결제상태"
