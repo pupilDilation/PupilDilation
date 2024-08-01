6d268edeb32a83b00cebbb5390db80e5ac99ae82
Jangmyun <cndlsrb2739@gmail.com>
2024-08-01 09:41:49 +0900

    [feat #89] node js app에서 cors를 사용하는 시점 변경

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require("path");
 const app = express();
 const port = 3001;
 const cors = require("cors");
+app.use(cors()); // cors 선언시점 변경
 
 //mysql2 db configuration file
 const db = require("./config/dbConfig");
@@ -42,7 +43,6 @@ sessionStore
 // client가 서버로 보내는 요청이 json 형식일 때
 // javascript 오브젝트로 변환해서 req.body 에 저장
 app.use(express.json());
-app.use(cors());
 // Routes 주소
 app.use("/concerts", concertRoutes);
 app.use("/users", userRoutes);
