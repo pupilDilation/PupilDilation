48810df85b4291a47a66fdb2118e1244340e8e16
hwanhojoung <hwanhodevelop@gmail.com>
2024-08-05 17:10:54 +0900

    [feat #113] footer page connection and create page

---
+++ pupil/src/pages/Terms.js
@@ -0,0 +1,12 @@
+import TermForm from "../components/Term/TermForm";
+import Wrapper from "../components/Wrapper/Wrapper";
+
+function Terms() {
+  return (
+    <Wrapper>
+      <TermForm />
+    </Wrapper>
+  );
+}
+
+export default Terms;
