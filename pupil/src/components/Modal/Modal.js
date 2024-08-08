64ade650291052c566f1fe8aa00cddd3f72893e0
Jangmyun <cndlsrb2739@gmail.com>
2024-08-08 13:32:07 +0900

    [feat #120] props로 받아온 modal창 open 여부 확인하여 modal 띄우기

--- pupil/src/components/Modal/Modal.js
+++ pupil/src/components/Modal/Modal.js
@@ -4,7 +4,10 @@ import modalStyles from "./Modal.module.css";
  * @author: Jangmyun
  * @description: overlay-뒷배경 rgba 어둡게 처리 / container안에 header와 content 분리하여 스타일링
  */
-function Modal() {
+function Modal(props) {
+  // props로 전달된 state로 모달창의 open 여부를 확인해서 null또는 modal 컴포넌트 return
+  if (!props.isOpened) return null;
+
   return (
     <div className={modalStyles.overlay} onClick={props.onClose}>
       <div className={modalStyles.container}>
