fea5811c69b415e9697a2e9451103e17d9310e3b
Jangmyun <cndlsrb2739@gmail.com>
2024-08-08 14:40:37 +0900

    [feat #120] modal 스타일 변경

--- pupil/src/components/Modal/Modal.js
+++ pupil/src/components/Modal/Modal.js
@@ -12,9 +12,9 @@ function Modal(props) {
     <div className={modalStyles.overlay} onClick={props.onClose}>
       <div className={modalStyles.container}>
         <div className={modalStyles.header}>
-          <h4>{props.title}</h4>
+          <h3>{props.title}</h3>
           <button onClick={props.onClose}>
-            <img src="/cross_circle.svg" alt="" width={30} />
+            <img src="/img/cross_circle.svg" alt="" width={20} />
           </button>
         </div>
         <div className={modalStyles.content}>{props.children}</div>
