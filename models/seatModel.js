a744609718d6a999b7c7b7d45132696aaa8b49b5
hhaminlee <hamin6899@naver.com>
2024-08-11 22:06:08 +0900

    [feat #130] session id로 콘서트 정보 가져오기 기능 추가

--- models/seatModel.js
+++ models/seatModel.js
@@ -16,15 +16,33 @@ const getSeatBySessionId = async (sessionId) => {
   return rows;
 };
 
+const getConcertInfoBySessionId = async (sessionId) => {
+  const [rows] = await db.query(
+    `SELECT c.concert_row, c.concert_col 
+     FROM concert c 
+     JOIN session s ON c.concert_id = s.concert_id 
+     WHERE s.session_id = ?`,
+    [sessionId]
+  );
+  return rows[0];
+};
+
 const updateSeatStatus = async (sessionId, seatNumber, seatStatus) => {
   await db.query(
     "UPDATE seat SET seat_status = ? WHERE session_id = ? AND seat_number = ?",
     [seatStatus, sessionId, seatNumber]
   );
+  //업데이트 된 모든 좌석 정보 반환
+  const [rows] = await db.query(
+    "SELECT seat_id, session_id, seat_status, seat_number FROM seat WHERE session_id = ?",
+    [sessionId]
+  );
+  return rows;
 };
 
 module.exports = {
   getSeatByConcertId,
   getSeatBySessionId,
+  getConcertInfoBySessionId,
   updateSeatStatus,
 };
