1d05050fddad1151011c419eb4d653a11f26adee
Jangmyun <cndlsrb2739@gmail.com>
2024-08-12 19:12:34 +0900

    [feat #136] getUserId via checkAuth get req

--- pupil/src/components/ChangePassword/ChangePassword.js
+++ pupil/src/components/ChangePassword/ChangePassword.js
@@ -1,29 +1,52 @@
 import { useNavigate, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { isLoggedIn } from "../../slice/loginSlice";
-import { setPassword, setPasswordChk } from "../../slice/signUpSlice";
-import { useEffect } from "react";
+import { setPw, setPwChk } from "../../slice/changePwSlice";
+import { useEffect, use, useState } from "react";
 import passwordStyles from "./ChangePassword.module.css";
-import SignUpFormStyles from "../SignUp/SignUpForm.module.css";
-import Input from "../Input/Input";
+import axios from "axios";
 
 function ChangePassword() {
+  const [userId, setUserId] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { params } = useParams();
 
+  const pw = useSelector((state) => state.changePw.pw);
+  const pwChk = useSelector((state) => state.changePw.pwChk);
+
+  async function getUserId() {
+    try {
+      const res = await axios.get("http://localhost:3001/auth/checkAuth", {
+        withCredentials: true,
+      });
+      if (res.data.authenticated) {
+        setUserId(res.data.userId);
+      }
+    } catch (error) {}
+  }
+
   return (
     <div className={passwordStyles.container}>
       <div className={passwordStyles.box}>
-        <label htmlFor="currentPw">현재 비밀번호</label>
-        <input id="currentPw" type="password" />
-        <br></br>
-        <br></br>
         <label htmlFor="changedPw">변경할 비밀번호</label>
-        <input id="changedPw" type="password" />
+        <input
+          id="changedPw"
+          type="password"
+          value={pw}
+          onChange={(e) => {
+            dispatch(setPw(e.target.value));
+          }}
+        />
         <br></br>
         <label htmlFor="changedPwChk">비밀번호 재입력</label>
-        <input id="changedPwChk" type="password" />
+        <input
+          id="changedPwChk"
+          type="password"
+          value={pwChk}
+          onChange={(e) => {
+            dispatch(setPwChk(e.target.value));
+          }}
+        />
         <br></br>
         <button>비밀번호 변경!</button>
       </div>
