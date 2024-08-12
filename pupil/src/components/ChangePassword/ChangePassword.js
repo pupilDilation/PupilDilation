5305dcffc0309ba010792d2e680cf374ae223ef3
Jangmyun <cndlsrb2739@gmail.com>
2024-08-12 14:28:01 +0900

    [feat #136] ChangePassword component 제작

---
+++ pupil/src/components/ChangePassword/ChangePassword.js
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { isLoggedIn } from "../../slice/loginSlice";
+
+function ChangePassword() {
+  return <div></div>;
+}
+
+export default ChangePassword;
