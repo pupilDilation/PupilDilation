23f4331be7c202df0b5e8bf4df15a4c366443272
Jangmyun <cndlsrb2739@gmail.com>
2024-08-12 16:19:22 +0900

    [feat #136] changepassword component basic layout

--- pupil/src/components/ChangePassword/ChangePassword.js
+++ pupil/src/components/ChangePassword/ChangePassword.js
@@ -1,9 +1,30 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { isLoggedIn } from "../../slice/loginSlice";
+import { setPassword, setPasswordChk } from "../../slice/signUpSlice";
+import { useEffect } from "react";
+import passwordStyles from "./ChangePassword.module.css";
+import SignUpFormStyles from "../SignUp/SignUpForm.module.css";
+import Input from "../Input/Input";
 
 function ChangePassword() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { params } = useParams();
+
+  return (
+    <div className={passwordStyles.container}>
+      <div className={passwordStyles.box}>
+        <label htmlFor="currentPw">현재 비밀번호</label>
+        <input id="currentPw" type="password" />
+        <label htmlFor="changedPw">변경할 비밀번호</label>
+        <input id="changedPw" type="password" />
+        <label htmlFor="changedPwChk">비밀번호 재입력</label>
+        <input id="changedPwChk" type="password" />
+        <button>비밀번호 변경!</button>
+      </div>
+    </div>
+  );
 }
 
 export default ChangePassword;
