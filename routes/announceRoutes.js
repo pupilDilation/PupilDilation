ef59119d579a88297992f2be734ec7a2f55944d9
Jangmyun <cndlsrb2739@gmail.com>
2024-08-13 18:26:58 +0900

    [feat #142] announcement post route

--- routes/announceRoutes.js
+++ routes/announceRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/all", announceController.getAllAnnounce);
 router.get("/3month", announceController.getAnnounce3MonthDesc);
+router.post("/newannounce", announceController.postAnnouncement);
 
 module.exports = router;
