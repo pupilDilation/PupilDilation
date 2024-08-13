13ff2d2d563ec99f561eba668a3ef3dc87e5ba6a
Jangmyun <cndlsrb2739@gmail.com>
2024-08-13 17:55:20 +0900

    [feat #142] getannounce3month controller

--- controllers/announceController.js
+++ controllers/announceController.js
@@ -12,10 +12,26 @@ const getAllAnnounce = async (req, res) => {
     res.json(announcements);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error });
+    res.status(500).json({ success: false, error: error });
+  }
+};
+
+const getAnnounce3MonthDesc = async (req, res) => {
+  try {
+    const announcements = await announceModel.getAnnounce3MonthDesc();
+    if (announcements.length < 1) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Announcement Not Found" });
+    }
+    return res.json(announcements);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: error });
   }
 };
 
 module.exports = {
   getAllAnnounce,
+  getAnnounce3MonthDesc,
 };
