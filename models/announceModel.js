fdc61634bef82860771b67f141aad5297ebdba6d
Jangmyun <cndlsrb2739@gmail.com>
2024-08-13 17:54:32 +0900

    [feat #142] 3달간 공지내역 불러오는 쿼리문 작성

--- models/announceModel.js
+++ models/announceModel.js
@@ -6,6 +6,17 @@ async function getAllAnnouncement() {
   return rows;
 }
 
+async function getAnnounce3MonthDesc() {
+  const [rows] = await db.query(`
+    SELECT *
+    FROM announcement
+    WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+    ORDER BY created_at DESC
+    `);
+  return rows;
+}
+
 module.exports = {
   getAllAnnouncement,
+  getAnnounce3MonthDesc,
 };
