25dadce74196f6fdfe1f7b4beb66b8776c9c287b
Jangmyun <cndlsrb2739@gmail.com>
2024-08-13 17:59:50 +0900

    [feat #142] 3month routes

--- routes/announceRoutes.js
+++ routes/announceRoutes.js
@@ -4,5 +4,6 @@ const announceController = require("../controllers/announceController");
 const router = express.Router();
 
 router.get("/all", announceController.getAllAnnounce);
+router.get("/3month", announceController.getAnnounce3MonthDesc);
 
 module.exports = router;
