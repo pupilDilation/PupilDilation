0d6ce107a557a891a87720aa197cbcea575dced4
Jangmyun <cndlsrb2739@gmail.com>
2024-08-13 13:49:05 +0900

    [feat #136] id 값 받아서 연결된 email로 변경링크 전송하는 post 요청

--- pupil/src/components/ChangePassword/ChangePasswordDefault.js
+++ pupil/src/components/ChangePassword/ChangePasswordDefault.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import axios from "axios";
 import passwordStyles from "./ChangePassword.module.css";
 
 function ChangePasswordDefault() {
+  const [id, setId] = useState("");
+
+  async function changePasswordClick(e) {
+    try {
+      const res = await axios.post("http://localhost:3001/auth/sendmail", {
+        userId: id,
+      });
+      if (res.data.success) {
+        alert("가입된 메일로 비밀번호 변경 링크가 전송되었습니다.");
+      }
+      console.log(res);
+    } catch (error) {
+      if (error.response.status === 404) {
+        alert("가입되지 않은 유저 아이디입니다.");
+      }
+      console.log(error);
+    }
+  }
+
   return (
     <div className={passwordStyles.container}>
       <div className={passwordStyles.box}>
         <label htmlFor="id">비밀번호 변경할 아이디를 입력해주세요.</label>
-        <input type="text" />
-        <button>이메일 전송!</button>
+        <input
+          id="id"
+          type="text"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <button onClick={changePasswordClick}>이메일 전송!</button>
       </div>
     </div>
   );
