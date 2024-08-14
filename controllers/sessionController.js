fc4b78ec6d18ced8727a05b7fab02b9d797daf62
248Kobe <ecjung077@gmail.com>
2024-08-14 15:53:21 +0900

    [feat #144] reservation, seats api 추가

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -35,7 +35,7 @@ const postSession = async (req, res) => {
     const result = await sessionModel.postSession(sessionData);
     res.status(201).json(result);
   } catch (error) {
-    res.status(500).json({ error: "Failed to create session." });
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -62,12 +62,18 @@ const deleteSession = async (req, res) => {
   const { concert_id, session_id } = req.params;
   try {
     const result = await sessionModel.deleteSession(concert_id, session_id);
-    if (result.affectedRows > 0) {
-      res.json(result);
+    if (result.sessionResult.affectedRows > 0) {
+      res.json({
+        success: true,
+        message: "Session and associated seats marked as deleted.",
+        sessionUpdate: result.sessionResult,
+        seatsUpdate: result.seatsResult,
+      });
     } else {
       res.status(404).json({ error: "Session not found." });
     }
   } catch (error) {
+    console.error("Failed to delete session:", error);
     res.status(500).json({ error: "Failed to delete session." });
   }
 };
