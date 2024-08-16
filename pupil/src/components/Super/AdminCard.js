514d6bab9d77cbb06c0bd67eb32c6758b5f76c6c
Jangmyun <cndlsrb2739@gmail.com>
2024-08-17 02:13:00 +0900

    [feat #155] AdminCard setup

--- pupil/src/components/Super/AdminCard.js
+++ pupil/src/components/Super/AdminCard.js
@@ -1,3 +1,11 @@
-function AdminCard() {
-  return <div></div>;
+import styles from "./Super.module.css";
+
+function AdminCard({ id, name, phone, email }) {
+  return (
+    <div className={styles.cardContainer}>
+      <p>{id}</p>
+    </div>
+  );
 }
+
+export default AdminCard;
