4b73bd7ee01729d5b3a3456cf601b89a31b94387
Jangmyun <cndlsrb2739@gmail.com>
2024-08-17 02:12:45 +0900

    [feat #155] Admin Account Controller Component setup

--- pupil/src/components/Super/AdminAccountController.js
+++ pupil/src/components/Super/AdminAccountController.js
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "./Super.module.css";
+
 function AdminAccountController() {
-  return <div></div>;
+  const [admins, setAdmins] = useState([]);
+  async function getAdminUser() {
+    try {
+      const res = await axios.get("http://localhost:3001/users/admins/get");
+      if (res.data.length > 0) {
+        setAdmins(res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching the JSON data:", error);
+    }
+  }
+  useEffect(() => {
+    getAdminUser();
+  }, []);
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          {admins.map((item) => {
+            console.log(item);
+            return <div key={item.user_id}>{item.user_id}</div>;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AdminAccountController;
