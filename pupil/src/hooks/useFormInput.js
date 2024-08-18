15df015f748edebb9584d119e8675af760db2d69
Jangmyun <cndlsrb2739@gmail.com>
2024-08-19 01:19:28 +0900

    [feat #155] 여러 개 인풋 값 state를 obj 타입으로 제어하는 함수

---
+++ pupil/src/hooks/useFormInput.js
@@ -0,0 +1,19 @@
+/**
+ * @author: Jangmyun
+ * @argument: initial obj state
+ * @return: [state, stateChangeFunction]
+ * @description: input이 여러 개일 때 input의 id값을 기준으로 input value 값을 state에 저장하고 해당 change 함수를 리턴하는 함수
+ */
+function useFormInput(initialState) {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  return [formData, handleChange];
+}
