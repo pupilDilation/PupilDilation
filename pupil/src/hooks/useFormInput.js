946af590a6bde4e810cda7ddc0cce42a17e642b3
Jangmyun <cndlsrb2739@gmail.com>
2024-08-19 01:47:17 +0900

    [fix #155] useFormInput export

--- pupil/src/hooks/useFormInput.js
+++ pupil/src/hooks/useFormInput.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 /**
  * @author: Jangmyun
  * @argument: initial obj state
  * @return: [state, stateChangeFunction]
  * @description: input이 여러 개일 때 input의 id값을 기준으로 input value 값을 state에 저장하고 해당 change 함수를 리턴하는 함수
  */
-function useFormInput(initialState) {
+export default function useFormInput(initialState) {
   const [formData, setFormData] = useState(initialState);
 
   const handleChange = (e) => {
