a8eb6b35895560c123af32e93d2078ca49b65144
Jangmyun <cndlsrb2739@gmail.com>
2024-08-18 22:03:45 +0900

    [feat #155] AdminCard Layout

--- pupil/src/components/Super/AdminCard.js
+++ pupil/src/components/Super/AdminCard.js
@@ -2,8 +2,20 @@ import styles from "./Super.module.css";
 
 function AdminCard({ id, name, phone, email }) {
   return (
-    <div className={styles.cardContainer}>
-      <p>{id}</p>
+    <div className={styles.adminCardWrapper}>
+      <div className={styles.cardContainer}>
+        <p>
+          <span>{id}</span>
+          <span>{name}</span>
+        </p>
+      </div>
+      <div className={styles.inputBox}>
+        <input type="text" id="username" placeholder="username" />
+        <input type="text" id="id" placeholder="id" />
+        <input type="text" id="password" placeholder="password" />
+        <input type="text" id="phone" placeholder="phone" />
+        <input type="text" id="email" placeholder="email" />
+      </div>
     </div>
   );
 }
