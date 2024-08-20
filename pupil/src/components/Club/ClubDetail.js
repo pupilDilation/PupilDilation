c04a3e2ce260855f94acf2614390b15e8e77e846
Jangmyun <cndlsrb2739@gmail.com>
2024-08-20 14:15:36 +0900

    [feat #162] clubId를 통해 디테일 정보 가져오는 axios get 요청함수 추가

--- pupil/src/components/Club/ClubDetail.js
+++ pupil/src/components/Club/ClubDetail.js
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ClubDetail.module.css";
+import { useParams } from "react-router";
+import axios from "axios";
 
-function ClubDetail(props) {
+function ClubDetail() {
+  const { clubId } = useParams();
+  const [clubDetail, setClubDetail] = useState([]);
+  async function getClubDetail() {
+    try {
+      const res = await axios.get(
+        `http://localhost:3001/club/clubid/${clubId}`
+      );
+      if (res.status === 404) {
+        alert("존재하지 않는 club입니다!");
+      } else {
+        setClubDetail(res.data[0]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getClubDetail();
+  }, []);
   return (
     <div className={styles.container}>
       <img className={styles.img} src="/img/logo/circle-logo.png" alt="" />
       <div className={styles.descbox}>
-        <h1>{props.clubName}</h1>
-        <p>{props.clubDesc}</p>
+        <h1>{clubDetail.club_name}</h1>
+        <p>{clubDetail.club_description}</p>
       </div>
     </div>
   );
