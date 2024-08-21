0549fd466f9e824d8c9d0ce907841d358f0a3105
Jangmyun <cndlsrb2739@gmail.com>
2024-08-21 14:35:44 +0900

    [feat #173] search component 레이아웃 완성

--- pupil/src/components/Search/Search.js
+++ pupil/src/components/Search/Search.js
@@ -1,5 +1,24 @@
-function Search() {
-  return <div></div>;
+import styles from "./Search.module.css";
+
+/**
+ * @author: Jangmyun
+ * @param: searchToggle,
+ */
+function Search(props) {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <img
+            src="/img/arrow-down.svg"
+            style={{ transform: "rotate(180deg)" }}
+            alt="arrow"
+            onClick={props.onClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
