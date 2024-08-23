8dc5233b78a214aea33f08dc03e55ba62ba3bc8a
Jangmyun <cndlsrb2739@gmail.com>
2024-08-23 14:09:55 +0900

    [feat #173] Link 태그로 해당 공연 디테일 페이지로 이동하도록 설정

--- pupil/src/components/Search/ConcertCard.js
+++ pupil/src/components/Search/ConcertCard.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import styles from "./Search.module.css";
 
-function ConcertCard({ title, imgUrl }) {
+function ConcertCard({ title, imgUrl, onClose, id }) {
   return (
     <div className={styles.concertCardContainer}>
-      {imgUrl ? (
-        <img src={imgUrl} alt={title} />
-      ) : (
-        <img src="/img/loadImage.png" alt={title} />
-      )}
-      <p>{title}</p>
+      <Link className={styles.link} to={`/details/${id}`} onClick={onClose}>
+        {imgUrl ? (
+          <img src={imgUrl} alt={title} />
+        ) : (
+          <img src="/img/loadImage.png" alt={title} />
+        )}
+        <p>{title}</p>
+      </Link>
     </div>
   );
 }
