62c13eec298c455a3e3adfed7dcd581766a58a0c
Jangmyun <cndlsrb2739@gmail.com>
2024-08-29 11:28:23 +0900

    [feat #181] seats post router

--- routes/seatRoutes.js
+++ routes/seatRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/concert/:concertId", seatController.getSeatByConcertId);
 router.get("/session/:sessionId", seatController.getSeatBySessionId);
-
+router.post("/", seatController.postSeats);
 router.put("/session/:sessionId", seatController.updateSeatStatus);
 
 module.exports = router;
