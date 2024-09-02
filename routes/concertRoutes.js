64dd8d31a7e611dba0a40a7c57ea3b460e156536
Jangmyun <cndlsrb2739@gmail.com>
2024-09-02 23:13:08 +0900

    [feat #191] getConcertsByClubId route

--- routes/concertRoutes.js
+++ routes/concertRoutes.js
@@ -7,6 +7,7 @@ router.get("/", concertController.getConcerts);
 router.get("/range", concertController.getConcertsInRange);
 router.get("/:concert_id", concertController.getConcertById);
 router.get("/user/:user_id", concertController.getConcertsByUserId);
+router.get("/club/:club_id", concertController.getConcertsByClubId);
 router.get("/get/concertbytitle", concertController.getConcertsByConcertName);
 router.post("/", concertController.createConcert);
 router.put("/:concert_id", concertController.putConcert);
